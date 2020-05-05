2b1301727b546627f75c0af972697d34fd9bb6d1
Olivia Hartzell <orh2106@columbia.edu>
2020-05-05 12:49:34 -0400

    updated home site with links to external sites

--- orh2106-final/app.py
+++ orh2106-final/app.py
@@ -20,8 +20,6 @@ app = Flask(__name__) #creates flask app variable
 #static route
 @app.route("/") #if go to website in there, and go to home route, will return whatever is defined in the following func
 def hello():
-    #return "<b> Test 123 </b>"
-    #we can take advanage of render template
     return render_template("index.html") # flask will look in templates directoryof current directory
     #return "Hello World!" #this is really html code here-
     #for external can just use href in the html file
@@ -31,17 +29,20 @@ def hello():
     #for the href-- "/columbia"
 
 
-#@app.route("/1006")
-#def my_site():
+@app.route("/1006-work")
+def my_site():
+    return render_template("1006_stuff.html")
  #   return "<p> This is my new site! </p>"
+ 
+ 
 
 
-#make a file that has alink from one page to another page
-    #use href attribute
+@app.route("/other-work")
+def new_site():
+    return render_template("other_stuff.html")
 
 
-#@app.route("/columbia")
-#def my_fun():
+
 
 #start the server
 if __name__ == "__main__":
